6dffb2976148cf7218dd6cd2ac41336273111a26
Ice Man <tshemm@gmail.com>
2020-01-05 15:12:13 +0200

    selecting a particular table to view now roughly working. need to remove the member ID from the results and to style the table. also need to check if javascript doesnt do this in a better way. solved the  giving duplicates problem by using a second parameter in mysqli_fetch_array()

--- profile.php
+++ profile.php
@@ -23,33 +23,7 @@ if($row = mysqli_fetch_assoc($result)) {
    }    
 }
 
-$formCategory="";
-// if view data was pressed form was submitted
-if( isset( $_POST['viewData'] ) ) {
-    // create variables
-    // wrap data with validate function
-    $formCategory = validateFormData( $_POST['category']);
-    
-    //if user did not select category
-    if( $formCategory=="") {
-        $errorMsg = "<div class='alert alert-danger'>Please select a category first.<a class='close' data-dismiss='alert'>&times;</a></div>";
-    }
 
-    //based on the category, select data from the appropriate table name and display it to the screen
-    $query = "SELECT COLUMN_NAME FROM information_schema.columns WHERE TABLE_NAME='member_register'";
-    $result = mysqli_query( $conn, $query );
-
-    if($result){
-        $columnNames = array();
-        $count = 0;
-        while ($row=   mysqli_fetch_array($result) ){
-            $columnNames[$count] = $row[0];
-            $count++;
-        }
-        $string = implode(',',$columnNames);
-        echo $string, '<br>';
-    }
-}
 // close the mysql connection
 mysqli_close($conn);
 
@@ -65,6 +39,7 @@ include('header.php');
                             <div class="form-group">
                                 <label for="userCategory">Select table to view</label>
                                 <?php
+                                //-------The dropdown list that shows the categories the user can pick from-------- 
                                     include('connection.php');
                                     $query= "SELECT category FROM categories INNER JOIN privileges ON categories.categoryID=privileges.categoryID AND privileges.memberID=".$_SESSION['user_id'];
                                     $result = mysqli_query( $conn, $query );
@@ -78,11 +53,71 @@ include('header.php');
                                         }
                                         echo "</select>";
                                     }
+                                ?>
+                                <button type="submit" class="btn btn-success btn-block" name="viewData">View data</button>
+
+                                <?php
+                                //----------The viewing of the data------------
+                                    $formCategory="";
+                                    // if view data was pressed form was submitted
+                                    if( isset( $_POST['viewData'] ) ) {
+                                        // create variables
+                                        // wrap data with validate function
+                                        $formCategory = validateFormData( $_POST['category']);
+                                        
+                                        //if user did not select category
+                                        if( $formCategory=="") {
+                                            $errorMsg = "<div class='alert alert-danger'>Please select a category first.<a class='close' data-dismiss='alert'>&times;</a></div>";
+                                            echo $errorMsg;
+                                            return;
+                                        }
+
+                                        //based on the category, select data from the appropriate table name and display it to the screen
+                                        $query = "SELECT COLUMN_NAME FROM information_schema.columns WHERE TABLE_NAME='$formCategory'";
+                                        $result = mysqli_query( $conn, $query );
+
+                                        if($result){
+                                            $columnNames = array();
+                                            $count = 0;
+                                            while ($row=   mysqli_fetch_array($result) ){
+                                                $columnNames[$count] = $row[0];
+                                                $count++;
+                                            }
+                                            //$string = implode(',',$columnNames);
+                                            //echo $string, '<br>';
+
+                                            //-------Create the table------- 
+
+                                            //the data rows
+                                            $row = array();
+                                            $query = "SELECT member_register.name, member_register.surname, $formCategory.* FROM $formCategory INNER JOIN member_register ON $formCategory.memberID=member_register.memberID";
+                                            $result = mysqli_query( $conn, $query );
+                                            if($result){
+                                                //table header
+                                                echo "<table style= 'width:100%'>";
+                                                echo "<tr>";
+                                                echo "<th>Name</th>";
+                                                echo "<th>Surname</th>";
+                                                echo "<th>".$columnNames[1]."</th>";
+                                                echo "<th>".$columnNames[2]."</th>";
+                                                echo "</tr>";
+
+                                                while($row = mysqli_fetch_array($result, MYSQLI_NUM)){
+                                                    echo "<tr>";
+                                                    for($i=0; $i<sizeof($row); $i++){
+                                                        echo "<td>".$row[$i]."</td>";
+                                                    }
+                                                    echo "</tr>";
+                                                }
+                                                echo "</table>";
+                                            }
+
+                                        }
+                                    }
                                     mysqli_close($conn);
                                 ?>
                             </div>
 
-                            <button type="submit" class="btn btn-success btn-block" name="viewData">View data</button>
                       </form>
                   </div>
                   <div class = "col-md-4 col-sm-4 col-xs-12"></div>
