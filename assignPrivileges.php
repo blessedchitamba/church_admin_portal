dbea9e536e7fa8f451e3414624126fc57bc0a729
Ice Man <tshemm@gmail.com>
2020-01-21 14:58:41 +0200

    group pastor assigning of privileges now works well, with the appropriate error handling to ensure a user exists in the member register before they can be entered.

--- assignPrivileges.php
+++ assignPrivileges.php
@@ -14,45 +14,66 @@ include('functions.php');
 //creating an account. 
 $errorMsg = "";
 $formInputs = array(); //array for our user inputs
+$invalidEntries = array();  //array for any invalid member entries
+$validEntries = array();  //array of valid entries
+$offices = array();
 
 $formName="";
 $formSurname="";
+$i = 1;
+$mult = 20;
 
 // if addData was pressed
 if( isset( $_POST['add'] ) ) {
     // connect to database
     include('connection.php');
 
-    // create variables
-    // wrap data with validate function
-    $formName = validateFormData( $_POST['name']);
-    $formSurname = validateFormData( $_POST['surname']);
+    //select all offices with null and push them into array
+    $query = "SELECT office FROM offices WHERE officer IS NULL";
+    $result = mysqli_query( $conn, $query );
+    while($row = mysqli_fetch_assoc($result)){
+      array_push($offices, $row['office']);
+    }
     
     //loop through the user inputs and validate them
-    for($i=1; $i<sizeof($columnNames); $i++){
-        $formInputs[$i-1] = validateFormData( $_POST[$i]);
-    }
+    for($k=1; $k<=4; $k++){
+        $formName = validateFormData( $_POST[$k]);
+        $formSurname = validateFormData($_POST[$mult*$k]);
 
-    //proceed if there are no password errors
-    if( $errorMsg=="") {
-	    
-        //do the insert of the data. select the appropriate memberID first
-        $columns = implode("`, `", $columnNames);
-        $inputs = implode("', '", $formInputs);
-        $query = "INSERT INTO ".$_SESSION['tb_name']."(`$columns`) VALUES (
-                    (SELECT memberID FROM member_register WHERE name = '$formName' AND surname='$formSurname'), 
-                    '$inputs')";
+        //continue if form name and surname are empty
+        if($formName=="" || $formSurname==""){ continue; }
+
+        //error handling in case group pastor types a non existant name
+        $query = "SELECT memberID FROM member_register WHERE name='$formName' AND surname='$formSurname' LIMIT 1";
         $result = mysqli_query( $conn, $query );
 
-        if( $result) {
-            echo "<div class='alert alert-success' role='alert'>Entry added successfully!<a class='close' data-dismiss='alert'>&times;</a></div>";
-        } else {
-             // something went wrong
+        if($result){
+            //if the member is in, run the update query
+            $row=   mysqli_fetch_array($result);
+            $query = "UPDATE offices SET officer=".$row['memberID']." WHERE office='".$offices[$k-1]."'";
+            $result = mysqli_query( $conn, $query );
+            if($result){
+                array_push($validEntries, $formName);
+            }else{
+                //if for any funny reason it did not go through
+                array_push($invalidEntries, $formName);
+            }
+        }else{
+            //add this name to array of invalid entries
             echo "Error: ". $query ."<br>" . mysqli_error($conn);
-        }
-	    
-	    // insert into table
-	   //hash the password
+            array_push($invalidEntries, $formName);
+        }      
+    }
+
+    //display success message of entered names
+    if(sizeof($validEntries)>0){
+      $names = implode(", ", $validEntries);
+      echo "<div class='alert alert-success' role='alert'>".$names." added successfully!<a class='close' data-dismiss='alert'>&times;</a></div>";
+    }
+    //after the loop, display an error message showing who needs to be re-entered
+    if(sizeof($invalidEntries)>0){
+      $names = implode(", ", $invalidEntries);
+      $errorMsg = "<div class='alert alert-danger'>".$names." could not be entered. Please ensure name and surname spellings are correct.<a class='close' data-dismiss='alert'>&times;</a></div>";
     }
     
 }
@@ -78,7 +99,7 @@ if(isset($conn)) {
                                 include('connection.php');
                                 $query = "SELECT office FROM offices WHERE officer IS NULL";
                                 $result = mysqli_query( $conn, $query );
-                                $i = 0;
+                                
                                 if(mysqli_num_rows($result) > 0 ){ 
                                   echo "<h3>Enter correct name and surname of each officer to be assigned below:</h3><br>";
                                   while($row = mysqli_fetch_assoc($result)){ 
@@ -89,9 +110,10 @@ if(isset($conn)) {
                                                       <input type='text' class='form-control' placeholder='Name' name='".$i."'>
                                                   </div>
                                                   <div class='col-md-6 col-sm-6 col-xs-6' style='padding-right: 0px; padding-left: 5px'>
-                                                      <input type='text' class='form-control' placeholder='Surname' name='".$i."'>
+                                                      <input type='text' class='form-control' placeholder='Surname' name='".$mult*$i."'>
                                                   </div>
                                             </div>";
+                                      $i++;
                                   }
                                 }else{
                                   //all offices are occupied
